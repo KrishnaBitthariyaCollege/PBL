7575daa0447d4bc334d25a28885c34c013a1f1be
KrishnaBitthariya <140392527+KrishnaBitthariya@users.noreply.github.com>
2025-10-26 01:31:37 +0530

    mpu6050 testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,30 @@
 #include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 
-
+Adafruit_MPU6050 mpu;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  Wire.begin();
+
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
 }
 
 void loop() {
-  
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
-}
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
 
+  Serial.print("Accel X: "); Serial.print(a.acceleration.x);
+  Serial.print(" Y: "); Serial.print(a.acceleration.y);
+  Serial.print(" Z: "); Serial.println(a.acceleration.z);
+  
+  delay(500);
+}
